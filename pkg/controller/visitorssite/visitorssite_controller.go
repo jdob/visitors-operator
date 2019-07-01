d4a75fa5300a03dc771bb833afcbd3b6e8a23803
jdob <jdobies@redhat.com>
2019-07-01 14:54:46 -0400

    Refactored out util methods for readability

--- pkg/controller/visitorssite/visitorssite_controller.go
+++ pkg/controller/visitorssite/visitorssite_controller.go
@@ -6,10 +6,8 @@ import (
 	visitorsv1alpha1 "github.com/jdob/visitors-operator/pkg/apis/visitors/v1alpha1"
 
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -174,80 +172,4 @@ func (r *ReconcileVisitorsSite) Reconcile(request reconcile.Request) (reconcile.
 
 	// Everything went fine, don't requeue
 	return reconcile.Result{}, nil
-}
-
-func (r *ReconcileVisitorsSite) ensureDeployment(request reconcile.Request,
-	instance *visitorsv1alpha1.VisitorsSite,
-	name string,
-	dep *appsv1.Deployment,
-) (*reconcile.Result, error) {
-
-	// See if deployment already exists and create if it doesn't
-	found := &appsv1.Deployment{}
-	err := r.client.Get(context.TODO(), types.NamespacedName{
-		Name:      name,
-		Namespace: instance.Namespace,
-	}, found)
-	if err != nil && errors.IsNotFound(err) {
-
-		// Create the deployment
-		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-		err = r.client.Create(context.TODO(), dep)
-
-		if err != nil {
-			// Deployment failed
-			log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-			return &reconcile.Result{}, err
-		} else {
-			// Deployment was successful
-			return nil, nil
-		}
-	} else if err != nil {
-		// Error that isn't due to the deployment not existing
-		log.Error(err, "Failed to get Deployment")
-		return &reconcile.Result{}, err
-	}
-
-	return nil, nil
-}
-
-func (r *ReconcileVisitorsSite) ensureService(request reconcile.Request,
-	instance *visitorsv1alpha1.VisitorsSite,
-	name string,
-	s *corev1.Service,
-) (*reconcile.Result, error) {
-	found := &corev1.Service{}
-	err := r.client.Get(context.TODO(), types.NamespacedName{
-		Name:      name,
-		Namespace: instance.Namespace,
-	}, found)
-	if err != nil && errors.IsNotFound(err) {
-
-		// Create the service
-		log.Info("Creating a new Service", "Service.Namespace", s.Namespace, "Service.Name", s.Name)
-		err = r.client.Create(context.TODO(), s)
-
-		if err != nil {
-			// Creation failed
-			log.Error(err, "Failed to create new Service", "Service.Namespace", s.Namespace, "Service.Name", s.Name)
-			return &reconcile.Result{}, err
-		} else {
-			// Creation was successful
-			return nil, nil
-		}
-	} else if err != nil {
-		// Error that isn't due to the service not existing
-		log.Error(err, "Failed to get Service")
-		return &reconcile.Result{}, err
-	}
-
-	return nil, nil
-}
-
-func labels(v *visitorsv1alpha1.VisitorsSite, tier string) map[string]string {
-	return map[string]string{
-		"app":             "visitors",
-		"visitorssite_cr": v.Name,
-		"tier":            tier,
-	}
 }
